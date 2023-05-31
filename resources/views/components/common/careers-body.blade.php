ab8cba45901eb8d4a1ed939a35df0609b373e1f5
MasterOfNothin <786.ali.nurudheen.3348@gmail.com>
2023-05-31 17:04:52 +0530

    latest

--- resources/views/components/common/careers-body.blade.php
+++ resources/views/components/common/careers-body.blade.php
@@ -3,6 +3,24 @@
 <p class=" font-montmedium text-sm md:text-base xl:text-lg mt-2 ">6 Months Training + 6 Months Internship With Payment</p>
 <p class=" font-montmedium text-sm md:text-base xl:text-lg mt-1">Only 6 Vacancies</p>
 <div>
+
+    <table class=" my-4 md:my-7 lg:my-10">
+        <tr>
+            <th>Particulars</th>
+            <th>Amount (Rs/Month)</th>
+            <th>Total</th>
+        </tr>
+        <tr>
+            <td>Fees for training (6 Months)</td>
+            <td>Rs 10,000/Month</td>
+            <td>60,000/-</td>
+        </tr>
+        <tr>
+            <td>Stipend for Internship (6 Months)</td>
+            <td>Rs 10,000/Month</td>
+            <td>60,000</td>
+        </tr>
+    </table>
     <p class=" font-montmedium text-sm md:text-base xl:text-lg mt-4 xl:mt-9">Fees For Training: Rs. 60,000/- (To be paid as Rs. 10000 / Month)</p>
     <p class=" font-montmedium text-sm md:text-base xl:text-lg mt-1">Remuneration for internship: Rs. 10,000/- per month (Total Rs. 60,000 for 6 months)</p>
     <p class=" font-montsemibold text-sm md:text-base xl:text-lg mt-1">Net Cost: Rs. 0.00/-</p>
--- resources/views/components/mobile/accordions.blade.php
+++ resources/views/components/mobile/accordions.blade.php
@@ -1,12 +1,12 @@
 
-<div id="accordion-collapse" data-accordion="collapse">
+<div id="accordion-collapse" data-accordion="collapse" x-data="{item1:false}">
     <h2 id="accordion-collapse-heading-1">
       <button type="button" class="flex items-center justify-between w-full p-5  text-left text-gray-500 border-2 mt-7 border-gray-400 focus:ring-4 focus:ring-gray-200  hover:bg-gray-100 " data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
         <span class="font-montmedium text-sm md:text-base xl:text-lg text-black">Is placement provided at the completion of the 1 year program?<br>ഒരു വർഷം കഴിയുമ്പോൾ placement നൽകുമോ?</span>
         <svg data-accordion-icon class="w-6 h-6 rotate-180 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
       </button>
     </h2>
-    <div id="accordion-collapse-body-1" class="hidden" aria-labelledby="accordion-collapse-heading-1">
+    <div id="accordion-collapse-body-1"  class="hidden w3-animate-zoom" aria-labelledby="accordion-collapse-heading-1">
       <div class="p-5 border-2 border-t-0 border-gray-400 ">
         <p class="mb-2 lg:mb-4 font-montmedium text-sm md:text-base xl:text-lg text-gray-600">Yes, placement is guaranteed. At the completion of 1 year term, you will be absorbed into our team as a Web Development Executive, subject to your good conduct and performance. Salary will be based on your performance during the internship period.</p>
         <p class="font-montmedium text-sm md:text-base xl:text-lg text-gray-600">തീർച്ചയായും. training & internship program സമയത്ത് താങ്കളുടെ പ്രകടനവും പ്രവർത്തന ശൈലിയും മോശമല്ലാത്തപക്ഷം, ഒരു വർഷത്തിന്റെ അവസാനം താങ്കളെ ഞങ്ങളുടെ തന്നെ development team ലേക്ക് ചേർക്കുന്നതായിരിക്കും.</p>
@@ -18,7 +18,7 @@
         <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
       </button>
     </h2>
-    <div id="accordion-collapse-body-2" class="hidden" aria-labelledby="accordion-collapse-heading-2">
+    <div id="accordion-collapse-body-2" class="hidden w3-animate-zoom" aria-labelledby="accordion-collapse-heading-2">
       <div class="p-5 border-2 border-t-0 border-gray-400 ">
         <p class="mb-2 lg:mb-4 font-montmedium text-sm md:text-base xl:text-lg text-gray-600">You may choose to quit any time. We don’t make you sign any bond or any legal agreement.</p>
         <p class="font-montmedium text-sm md:text-base xl:text-lg text-gray-600">താങ്കൾക്ക് തുടരാൻ താല്പര്യം ഇല്ലെങ്കിൽ എപ്പോൾ വേണമെങ്കിലും ഈ program ൽ നിന്നും പിന്മാറാവുന്നതാണ്. അതിൽ നിന്നും താങ്കളെ വിലക്കുന്ന യാതൊരു വിധ ബോണ്ടും എഗ്രിമെന്റും ഒപ്പിടേണ്ടതില്ല.</p>
@@ -30,7 +30,7 @@
         <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
       </button>
     </h2>
-    <div id="accordion-collapse-body-3" class="hidden" aria-labelledby="accordion-collapse-heading-3">
+    <div id="accordion-collapse-body-3" class="hidden w3-animate-zoom" aria-labelledby="accordion-collapse-heading-3">
       <div class="p-5 border-2 border-t-0 border-gray-400 ">
         <p class="mb-2 lg:mb-4 font-montmedium text-sm md:text-base xl:text-lg text-gray-600">Yes, if you complete a minimum period of 6 months with us starting from your date of joining the course, we will provide you with an experience certificate for the period you have been with us.</p>
         <p class="font-montmedium text-sm md:text-base xl:text-lg text-gray-600">നിങ്ങൾ ഈ program ൽ കുറഞ്ഞത് 6 മാസം പൂർത്തിയാക്കിയിട്ടുണ്ടെങ്കിൽ നിങ്ങൾക്ക് പൂർത്തിയാക്കിയ കാലയളവിന്റെ experience certificate നൽകുന്നതാണ്.</p>
@@ -44,7 +44,7 @@
           <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
         </button>
       </h2>
-      <div id="accordion-collapse-body-4" class="hidden" aria-labelledby="accordion-collapse-heading-3">
+      <div id="accordion-collapse-body-4" class="hidden w3-animate-zoom" aria-labelledby="accordion-collapse-heading-3">
         <div class="p-5 border-2 border-t-0 border-gray-400 ">
           <p class="mb-2 lg:mb-4 font-montmedium text-sm md:text-base xl:text-lg text-gray-600">This is absolutely real. We are a start-up private limited company and this is a part of our team building process.
             Our objective is to recruit freshers, provide free training for them and develop them as our core team members.
@@ -62,7 +62,7 @@
           <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
         </button>
       </h2>
-      <div id="accordion-collapse-body-5" class="hidden" aria-labelledby="accordion-collapse-heading-5">
+      <div id="accordion-collapse-body-5" class="hidden w3-animate-zoom" aria-labelledby="accordion-collapse-heading-5">
         <div class="p-5 border-2 border-t-0 border-gray-400 ">
           <p class="mb-2 lg:mb-4 font-montmedium text-sm md:text-base xl:text-lg text-gray-600">We are not a training institute. We are a software development company. We are conducting this program to recruit and train our own team members. Currently, we only plan to take 6 members to our team. So, that is why we have restricted the numbers to just 6.
             This way, we make sure that we are able to provide placement for all the trainees in our own team.
@@ -81,7 +81,7 @@
           <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
         </button>
       </h2>
-      <div id="accordion-collapse-body-6" class="hidden" aria-labelledby="accordion-collapse-heading-6">
+      <div id="accordion-collapse-body-6" class="hidden w3-animate-zoom" aria-labelledby="accordion-collapse-heading-6">
         <div class="p-5 border-2 border-t-0 border-gray-400 ">
           <p class="mb-2 lg:mb-4 font-montmedium text-sm md:text-base xl:text-lg text-gray-600">Our key benefit we expect is to build a fresh team of enthusiastic developers. In monetary terms, we get to benefit out of the software projects that the team of interns will be handling. Thus, it is a win-win situation for all.
             </p>
@@ -98,7 +98,7 @@
           <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
         </button>
       </h2>
-      <div id="accordion-collapse-body-7" class="hidden" aria-labelledby="accordion-collapse-heading-7">
+      <div id="accordion-collapse-body-7" class="hidden w3-animate-zoom" aria-labelledby="accordion-collapse-heading-7">
         <div class="p-5 border-2 border-t-0 border-gray-400 ">
           <p class="mb-2 lg:mb-4 font-montmedium text-sm md:text-base xl:text-lg text-gray-600">The selection is based on an online aptitude test and interview. Click on the link below to apply and get link to the aptitude test. Once you provide the details in the given form, the link to the aptitude test will be mailed to you.
             The test is of just 20 minutes duration. You can complete it any time before 10th of June. Shortlisted candidates will be intimated about further steps by phone and email.
