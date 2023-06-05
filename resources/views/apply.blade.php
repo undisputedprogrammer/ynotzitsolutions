5826573b51ecf07e472d5520bb07d70fc1253ab9
MasterOfNothin <786.ali.nurudheen.3348@gmail.com>
2023-06-05 16:20:26 +0530

    form updated

--- resources/views/apply.blade.php
+++ resources/views/apply.blade.php
@@ -79,7 +79,7 @@ $isblog=false;
                       <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="education" name="qualification">
                         <option value="PG">Post Graduate</option>
 
-                        <option value="UG">Under Graduate</option>
+                        <option value="UG">Graduate</option>
 
                         <option value="DIP">Diploma</option>
 
