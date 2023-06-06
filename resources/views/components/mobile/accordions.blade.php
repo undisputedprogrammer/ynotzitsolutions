391c5b13f6e5ee63d380d0639181a7b79b6f7f97
MasterOfNothin <786.ali.nurudheen.3348@gmail.com>
2023-06-06 16:50:33 +0530

    edit

--- resources/views/components/mobile/accordions.blade.php
+++ resources/views/components/mobile/accordions.blade.php
@@ -42,7 +42,7 @@
       </div>
 
 
-      <h2 id="accordion-collapse-heading-4" class="accordion">
+      {{-- <h2 id="accordion-collapse-heading-4" class="accordion">
         <button type="button" class="flex items-center justify-between w-full mt-2 lg:mt-4 p-5  text-left text-gray-500 border-2 border-gray-400 focus:ring-4 focus:ring-gray-200  hover:bg-gray-100 " data-accordion-target="#accordion-collapse-body-4" aria-expanded="false" aria-controls="accordion-collapse-body-4">
           <span class="font-montmedium text-sm md:text-base xl:text-lg text-black">Is this real? Why are we doing it free of cost?<br><span>ഇത് ശരിക്കും ഉള്ളതാണോ? എന്തിനാണ് ഞങ്ങൾ training നൽകുന്നത്?</span></span>
           <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
@@ -58,7 +58,7 @@
             </p>
 
         </div>
-      </div>
+      </div> --}}
 
 
       <h2 id="accordion-collapse-heading-5" class="accordion">
