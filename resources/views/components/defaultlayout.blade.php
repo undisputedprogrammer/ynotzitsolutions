70becbda6b52d881457650d6ec885e9b3c41b13c
MasterOfNothin <786.ali.nurudheen.3348@gmail.com>
2023-06-07 13:05:38 +0530

    meta code edited

--- resources/views/components/defaultlayout.blade.php
+++ resources/views/components/defaultlayout.blade.php
@@ -23,14 +23,13 @@
     t.src=v;s=b.getElementsByTagName(e)[0];
     s.parentNode.insertBefore(t,s)}(window, document,'script',
     'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '1001543091216869');
+    fbq('init', '804649157949495');
     fbq('track', 'PageView');
     </script>
     <noscript><img height="1" width="1" style="display:none"
-    src="https://www.facebook.com/tr?id=1001543091216869&ev=PageView&noscript=1"
+    src="https://www.facebook.com/tr?id=804649157949495&ev=PageView&noscript=1"
     /></noscript>
     <!-- End Meta Pixel Code -->
-
 
 </head>
 
